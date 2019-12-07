5af5d5d9725ad5bd27516d7ca4da3f2290628064
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-07 06:15:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+
+def reverse_each_word(array)
+  phrase_array=array.split
+  reverse_a=phrase_array.each do |word|
+    word.reverse!
+  end
+  return reverse_a.join(" ")
+end
+
+
+def reverse_each_word(array)
+  phrase_array=array.split
+  reverse_a=phrase_array.collect {|word| word.reverse!}.join(" ")
+end
