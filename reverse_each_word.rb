a3308cd36f0aa7949b4ed4ba3afd32b2fb723fad
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-07 06:00:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 
 def reverse_each_word(array)
   phrase_array=array.split
-  reverse=phrase_array.each do |word|
-    
-  
+  reverse_a=phrase_array.each do |word|
+    word.reverse
+  return reverse_a.join
+end
