ee8756e522e10276faa655935e6fba517016287c
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-07 05:54:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(array)
 array=[]
 reverse=
 phrase= "Hello there, and how are you?"
-array=phrase.split
+array=p"Hello there, and how are you?".split
 array.each do {|word| word.reverse}
 end
 end
